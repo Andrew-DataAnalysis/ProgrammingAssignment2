0287df7efdac3bf184cf6c204d8fc9b180cf5a0e
Andrew Lyons <alyons23@gmail.com>
2017-06-28 21:13:59 +0200

    Completed version of the cache matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    #Reads in the initial matrix and sets up the paramerts to be cached
+    
+    #This section initialises variables
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    #This initialises the functions
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    #Assigns the functions to variables in the returned list
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
@@ -12,4 +28,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    #Gets the cached version
+    m <- x$getsolve()
+    
+    #If a cached version exists, return it
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    #Otherwise, gets the matrix from the get function, within makeCacheMatrix
+    data <- x$get()
+    #Performs the inverse calculation
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
